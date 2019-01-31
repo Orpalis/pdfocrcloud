d6b803acd13017f6e21e37a6b29fa689ab7350b3
ecarrere <e.carrere@orpalis.com>
2019-01-31 11:32:22 +0100

    Added a panel to each option menu tab

--- Views/frmOptions.Designer.cs
+++ Views/frmOptions.Designer.cs
@@ -29,29 +29,51 @@
         private void InitializeComponent()
         {
             this.tabOCR = new System.Windows.Forms.TabPage();
-            this.chkSkipPagesWithText = new System.Windows.Forms.CheckBox();
-            this.cboOcrLanguage = new System.Windows.Forms.ComboBox();
             this.lbOcrLanguage = new System.Windows.Forms.Label();
+            this.cboOcrLanguage = new System.Windows.Forms.ComboBox();
+            this.chkSkipPagesWithText = new System.Windows.Forms.CheckBox();
+            this.panelOCR = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
-            this.tabGeneral.SuspendLayout();
             this.tabOCR.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabOCR);
+            this.tabControl1.Controls.SetChildIndex(this.tabOCR, 0);
+            this.tabControl1.Controls.SetChildIndex(this.tabGeneral, 0);
             // 
             // tabOCR
             // 
             this.tabOCR.Controls.Add(this.lbOcrLanguage);
             this.tabOCR.Controls.Add(this.cboOcrLanguage);
             this.tabOCR.Controls.Add(this.chkSkipPagesWithText);
+            this.tabOCR.Controls.Add(this.panelOCR);
             this.tabOCR.Location = new System.Drawing.Point(4, 22);
             this.tabOCR.Name = "tabOCR";
-            this.tabOCR.Size = new System.Drawing.Size(471, 202);
+            this.tabOCR.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOCR.Size = new System.Drawing.Size(485, 217);
             this.tabOCR.TabIndex = 2;
             this.tabOCR.Text = "OCR";
             this.tabOCR.UseVisualStyleBackColor = true;
+            // 
+            // lbOcrLanguage
+            // 
+            this.lbOcrLanguage.AutoSize = true;
+            this.lbOcrLanguage.Location = new System.Drawing.Point(15, 15);
+            this.lbOcrLanguage.Name = "lbOcrLanguage";
+            this.lbOcrLanguage.Size = new System.Drawing.Size(84, 13);
+            this.lbOcrLanguage.TabIndex = 13;
+            this.lbOcrLanguage.Text = "OCR Language:";
+            // 
+            // cboOcrLanguage
+            // 
+            this.cboOcrLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOcrLanguage.FormattingEnabled = true;
+            this.cboOcrLanguage.Location = new System.Drawing.Point(102, 9);
+            this.cboOcrLanguage.Name = "cboOcrLanguage";
+            this.cboOcrLanguage.Size = new System.Drawing.Size(112, 21);
+            this.cboOcrLanguage.TabIndex = 12;
             // 
             // chkSkipPagesWithText
             // 
@@ -65,37 +87,23 @@
             this.chkSkipPagesWithText.Text = "Skip pages having text";
             this.chkSkipPagesWithText.UseVisualStyleBackColor = true;
             // 
-            // cboOcrLanguage
-            //
-            this.cboOcrLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboOcrLanguage.FormattingEnabled = true;
-            this.cboOcrLanguage.Location = new System.Drawing.Point(105, 12);
-            this.cboOcrLanguage.Name = "cboOcrLanguage";
-            this.cboOcrLanguage.Size = new System.Drawing.Size(112, 21);
-            this.cboOcrLanguage.TabIndex = 12;
+            // panelOCR
             // 
-            // lbOcrLanguage
-            // 
-            this.lbOcrLanguage.AutoSize = true;
-            this.lbOcrLanguage.Location = new System.Drawing.Point(15, 15);
-            this.lbOcrLanguage.Name = "lbOcrLanguage";
-            this.lbOcrLanguage.Size = new System.Drawing.Size(84, 13);
-            this.lbOcrLanguage.TabIndex = 13;
-            this.lbOcrLanguage.Text = "OCR Language:";
+            this.panelOCR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelOCR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelOCR.Location = new System.Drawing.Point(3, 3);
+            this.panelOCR.Name = "panelOCR";
+            this.panelOCR.Size = new System.Drawing.Size(479, 211);
+            this.panelOCR.TabIndex = 14;
             // 
             // frmOptions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(497, 344);
             this.Name = "frmOptions";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "PassportPDF PDF OCR Cloud - Options";
             this.tabControl1.ResumeLayout(false);
-            this.tabGeneral.ResumeLayout(false);
-            this.tabGeneral.PerformLayout();
             this.tabOCR.ResumeLayout(false);
             this.tabOCR.PerformLayout();
             this.ResumeLayout(false);
@@ -107,5 +115,6 @@
         private System.Windows.Forms.CheckBox chkSkipPagesWithText;
         private System.Windows.Forms.ComboBox cboOcrLanguage;
         private System.Windows.Forms.Label lbOcrLanguage;
+        private System.Windows.Forms.Panel panelOCR;
     }
 }
